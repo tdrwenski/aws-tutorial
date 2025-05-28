65b5e5723e1edaaa6f2fc736099474b4391161d5
Tara Drwenski <drwenski1@llnl.gov>
2025-05-28 17:34:19 -0600

    Update script to clean up zips

--- submit-lambdas.sh
+++ submit-lambdas.sh
@@ -3,6 +3,7 @@
 # launch function
 zip launch-function.zip launch-function.py
 aws s3 cp launch-function.zip s3://hpcic-tutorials/slackbot/launch-function.zip
+rm launch-function.zip
 
 # notify function
 mkdir -p notify-package
@@ -11,3 +12,4 @@ cp notify-function.py notify-package/
 zip -r notify-function.zip notify-package
 aws s3 cp notify-function.zip s3://hpcic-tutorials/slackbot/notify-function.zip
 rm -rf notify-package
+rm notify-function.zip
