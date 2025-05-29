bd61b30ad4a18a915bc75990862a15fc49f84496
Tara Drwenski <drwenski1@llnl.gov>
2025-05-28 18:04:23 -0600

    Fix script

--- submit-lambdas.sh
+++ submit-lambdas.sh
@@ -6,10 +6,9 @@ aws s3 cp launch-function.zip s3://hpcic-tutorials/slackbot/launch-function.zip
 rm launch-function.zip
 
 # notify function
-mkdir -p notify-package
-python3 -m pip install requests -t notify-package
-cp notify-function.py notify-package/
-zip -r notify-function.zip notify-package
+mkdir requests
+python3 -m pip install requests -t requests --no-cache-dir
+zip -r notify-function.zip notify-function.py requests*
 aws s3 cp notify-function.zip s3://hpcic-tutorials/slackbot/notify-function.zip
-rm -rf notify-package
+rm -rf requests*
 rm notify-function.zip
